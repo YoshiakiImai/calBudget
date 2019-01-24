634dd12674a68e3b4ee9364b28fc86753e111a45
ip-publishing <y.imai0929@gmail.com>
2019-01-24 14:26:01 +0900

    Update app.js
    controllerモジュール内にサイト内のイベントに反応するaddEventListnerメソッドを追加してマウスでクリックした場合とエンターキーを押した場合に反応するよう実装。変数CtrlAddItemでそれぞれのイベントリスナーのパラメーターへ渡している。

--- app.js
+++ app.js
@@ -11,39 +11,48 @@
 * 	・イベント操作の追加
 */
 
+//データモジュール
 var budgetController = (function(){
 	
-	var x = 23;
-	
-	var add = function(a){
-		return x + a;
-	}
-	
-	return{
-		publicTest: function(b){
-			return add(b);
-		}
-	}
-					
+    // some code
+			
 })();
 
-
+//ユーザーインターフェイスモジュール
 var UIController = (function(){
 
 	// some code
 	
 })();
 
-//BudgetControllerとUIControllerを統括するモジュール
+//BudgetControllerとUIControllerを統括するコントロールモジュール
 var controller =(function (budgetCtrl, UICtrl){
 	
-	var z = budgetCtrl.publicTest(5);
-	
-	return {
-		anotherPublic: function() {
-			console.log(z);
-		}
+	var ctrlAddItem = function(){
+		
+		// 1. Get the field input data
+		
+		// 2. Add the item to the budget controller
+		
+		// 3. Add the item to the UI
+		
+		// 4. Calculate the budget
+		
+		// 5. Display the budget on the UI
+
+		console.log('it works.');
 	}
+	
+	//マウスでクリックをした場合
+	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+	
+	//エンターキーを押した場合
+	document.addEventListener('keypress', function(event) {
+		
+		if(event.key === 13 || event.which === 13){
+		ctrlAddItem();
+		}
+	});
 	
 })(budgetController,UIController);
 
