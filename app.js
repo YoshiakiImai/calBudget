5481ea7006826695ddcc0fe4e7cf9fca449ff7e4
ip-publishing <y.imai0929@gmail.com>
2019-01-24 17:21:32 +0900

    Update app.js
    HTMLとJSを関連付けさせる為のオブジェクトを生成、DOMstringsオブジェクト内に各ターゲットのタグを記載。
    getInputへ各ユーザーからの収支の種類と説明、値段の入力を格納するための配列変数を返す。

--- app.js
+++ app.js
@@ -21,18 +21,45 @@ var budgetController = (function(){
 //ユーザーインターフェイスモジュール
 var UIController = (function(){
 
-	// some code
+	//HTMLとJSを関連付けさせるオブジェクトを生成
+	var DOMstrings = {
+		inputType: '.add__type',
+		inputDescription: '.add__description',
+		inputValue: '.add__value',
+		inputBtn: '.add__btn'
+	}
+	
+	return {
+		getInput: function() {
+			return{
+					type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+					description: document.querySelector(DOMstrings.inputDescription).value,
+					value: document.querySelector(DOMstrings.inputValue).value
+			};
+		},
+		
+		//DOMをクロージャーよりパブリックに公開
+		getDOMstrings: function() {
+			return DOMstrings;	
+		}
+	};
 	
 })();
 
 //BudgetControllerとUIControllerを統括するコントロールモジュール
 var controller =(function (budgetCtrl, UICtrl){
 	
+	//UICtrl内のDOMstringsをコール
+	var DOM = UICtrl.getDOMstrings();
+	
 	var ctrlAddItem = function(){
 		
 		// 1. Get the field input data
+		var input = UICtrl.getInput();
+		console.log(input);
 		
 		// 2. Add the item to the budget controller
+		
 		
 		// 3. Add the item to the UI
 		
@@ -40,18 +67,18 @@ var controller =(function (budgetCtrl, UICtrl){
 		
 		// 5. Display the budget on the UI
 
-		console.log('it works.');
 	}
 	
 	//マウスでクリックをした場合
-	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+	document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 	
 	//エンターキーを押した場合
 	document.addEventListener('keypress', function(event) {
 		
-		if(event.key === 13 || event.which === 13){
-		ctrlAddItem();
+		if(event.keycode === 13 || event.which === 13){
+			ctrlAddItem();
 		}
+		
 	});
 	
 })(budgetController,UIController);
