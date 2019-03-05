551909fd8aa67730451bc9fa51340d817843ae42
ModernTechGames <modern.tech.game@gmail.com>
2019-03-05 18:18:50 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 /*
 *calBudgetアプリケーションを3つのモジュールで構成されるよう設計する。
-*　１ユーザーインターフェイス（UI）モジュール
+*　１データモジュール　
+* 	・新規に入力されたデータを蓄積する
+*	・入力の値の演算
+*　２ユーザーインターフェイス（UI）モジュール
 *	・ユーザーからの入力表示
 *	・入力、演算後のUIへの更新情報の表示
 *	・更新情報をUIへ表示
-*　２データモジュール　
-* 	・新規に入力されたデータを蓄積する
-*	・入力の値の演算
 *　３コントローラーモジュール
 * 	・イベント操作の追加
 */
@@ -87,7 +87,9 @@ var UIController = (function(){
 		inputType: '.add__type',
 		inputDescription: '.add__description',
 		inputValue: '.add__value',
-		inputBtn: '.add__btn'
+		inputBtn: '.add__btn',
+		incomeContainer: '.income__list',
+		expensesContainer: '.expenses__list'
 	}
 	
 	return {
@@ -99,7 +101,47 @@ var UIController = (function(){
 			};
 		},
 		
-		//DOMをクロージャーよりパブリックに公開
+		//HTML初期設定に入力された値を代入する
+		addListItem: function(obj, type) {
+			var html, newHtml, element;
+			
+			// HTML初期設定
+			if(type === 'inc') {
+				element = DOMstrings.incomeContainer;
+				html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+			} else if (type === 'exp') {
+				element = DOMstrings.expensesContainer;
+				html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+			}
+			 			
+			// 入力された値を代入する
+			newHtml = html.replace('%id%', obj.id);
+			//IDを書き換えたため新たなHTMLへ入力値を代入
+			newHtml = newHtml.replace('%description%', obj.description);
+			newHtml = newHtml.replace('%value%', obj.value);
+			
+			// DOMで指定された収支のコンテイナタグへ代入された値を挿入する
+			document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+			
+			
+		},
+		
+		clearFields: function(){
+			var fields, fieldsArr;
+			
+			//DOMstringの収支内容と値を変数fieldsへ格納
+			fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+			//fieldsの値を配列fieldsArrヘ呼び出す
+			fieldsArr = Array.prototype.slice.call(fields);
+			//current.valueの値をnull値へ設定
+			fieldsArr.forEach(function(current, index, array){
+				current.value = "";
+			});
+			
+			fieldsArr[0].focus();			
+		},
+		
+		//ドキュメントオブジェクトモデルDOMをクロージャーよりパブリックに公開
 		getDOMstrings: function() {
 			return DOMstrings;	
 		}
@@ -126,7 +168,15 @@ var controller =(function (budgetCtrl, UICtrl){
 		}
 	});
 	};
-
+	
+	var updateBudget = function(){
+		
+		// 1. Calculate the budget
+		
+		// 2. return the budget
+		
+		// 5. Display the budget on the UI
+	};
 	
 	var ctrlAddItem = function(){
 		var input, newItem;
@@ -135,15 +185,17 @@ var controller =(function (budgetCtrl, UICtrl){
 		input = UICtrl.getInput();
 		
 		
-		// 2. 上記inputへ格納されたパラメータの値を挿入
+		// 2. 上記inputへ格納されたパラメータのオブジェクトを挿入
 		newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 		
 		
-		// 3. Add the item to the UI
+		// 3. UICtrlのaddListenItemを呼び出しオブジェクトを引数として収支の種類をinput.typeで判断させ値を返す
+		UICtrl.addListItem(newItem, input.type);
 		
-		// 4. Calculate the budget
+		// 4. ユーザーより事前に入力ストアされた値を削除する関数
+		UICtrl.clearFields();
 		
-		// 5. Display the budget on the UI
+		
 
 	};
 	
